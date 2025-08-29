b328251d5c832d9e480d4a5b1cc180b8924e8cd9
Yoiner Pertuz <164366734+YoinerPer0001@users.noreply.github.com>
2025-08-28 19:04:38 -0500

    Update main.py

--- activity1/main.py
+++ activity1/main.py
@@ -3,39 +3,34 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-  
 
-# fetch dataset 
+# Fetch dataset 
 iris = fetch_ucirepo(id=53) 
 
-#numero de vecinos
+# Separar características (X) y etiquetas (y)
+X = iris.data.features
+y = iris.data.targets.values.ravel()  # Convertir a vector plano si es necesario
+
+# Número de vecinos
 k = 3
 
-#nuevo punto a clasificar
-nuevo_punto = [5.0, 3.5, 1.5, 0.2]
+# Nuevos puntos a clasificar
+nuevos_puntos = [
+    [5.0, 3.5, 1.5, 0.5],
+    [5.0, 3.5, 2.0, 0.2],
+    [5.0, 2.1, 1.5, 0.1],
+    [5.0, 3.5, 1.5, 0.2]
+]
 
-# Graficar dataset en 2D
-plt.figure(figsize=(8, 6))
-
-X = iris.data.features 
-y = iris.data.targets
-
-df = pd.DataFrame({
-    "Característica 1": X.iloc[:, 0],
-    "Característica 2": X.iloc[:, 1],
-    "Target": y.iloc[:, 0]
-})
-
-
-
-# Crear y entrenar el modelo KNN con
+# Crear y entrenar el modelo KNN
 knn = KNeighborsClassifier(n_neighbors=k)
 knn.fit(X, y)
 
+# Clasificar cada nuevo punto
+for nuevo_punto in nuevos_puntos:
+    prediccion = knn.predict([nuevo_punto])[0]
+    vecinos = knn.kneighbors([nuevo_punto], return_distance=False)[0]
 
-prediccion = knn.predict([nuevo_punto])[0]
-vecinos = knn.kneighbors([nuevo_punto], return_distance=False)[0]
-
-# Imprimir información
-print(f"Clase predicha para el nuevo punto {nuevo_punto}: Clase {prediccion}")
-print(f"IDs (índices) de los 3 vecinos más cercanos: {vecinos}")
+    # Imprimir información
+    print(f"Clase predicha para el nuevo punto {nuevo_punto}: Clase {prediccion}")
+    print(f"IDs (índices) de los {k} vecinos más cercanos: {vecinos}")
